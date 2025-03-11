c3d65560109e9b3361a559dd64b84ca9d2ea26a2
Nerim <nerim5037@gmail.com>
2025-03-11 20:22:29 +0900

    feat: Navi Bar 초기 구현

---
+++ src/app/nav/NavProfileCard.tsx
@@ -0,0 +1,59 @@
+import Image from "next/image";
+import styles from "./styles.module.css";
+import Link from "next/link";
+
+export default function NavProfileCard() {
+  return (
+    <div className={styles.profileCardContainer}>
+      <div className={styles.profileImg}>
+        <Image src="/images/no_profileImg.svg" fill alt="프로필 이미지" />
+      </div>
+      <ul className={styles.menuContainer}>
+        <li className={styles.menu}>
+          <Link href="/mypage">
+            <Image
+              src="/images/icon_menu1.svg"
+              width="24"
+              height="24"
+              alt="내 정보"
+            />
+            내 정보
+          </Link>
+        </li>
+        <li className={styles.menu}>
+          <Link href="/reservation">
+            <Image
+              src="/images/icon_menu2.svg"
+              width="24"
+              height="24"
+              alt="예약 내역"
+            />
+            예약 내역
+          </Link>
+        </li>
+        <li className={styles.menu}>
+          <Link href="/activities">
+            <Image
+              src="/images/icon_menu3.svg"
+              width="24"
+              height="24"
+              alt="내 체험 관리"
+            />
+            내 체험 관리
+          </Link>
+        </li>
+        <li className={styles.menu}>
+          <Link href="">
+            <Image
+              src="/images/icon_menu4.svg"
+              width="24"
+              height="24"
+              alt="예약 현황"
+            />
+            예약 현황
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
---
+++ src/app/nav/page.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import Image from "next/image";
+import styles from "./styles.module.css";
+import Link from "next/link";
+import { useEffect, useRef, useState } from "react";
+import NavProfileCard from "./NavProfileCard";
+
+export default function nav() {
+  const [isProfileCard, setIsProfileCard] = useState(false);
+  const profileRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (
+        profileRef.current &&
+        !profileRef.current.contains(event.target as Node)
+      ) {
+        setIsProfileCard(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <nav className={styles.nav}>
+      <div className={styles.navContainer}>
+        <div className={styles.logo}>
+          <Link href="/">
+            <Image
+              src="/images/nav_globalNomad.svg"
+              width="172"
+              height="30"
+              alt="Global Nomad"
+            />
+          </Link>
+        </div>
+        {/* 로그인 전 */}
+        {/* <div className={styles.noTokenUI}>
+          <Link href="/signin">로그인</Link>
+          <Link href="/signup">회원가입</Link>
+        </div> */}
+        {/* 로그인 후 */}
+        <div className={styles.hasTokenUI}>
+          <div className={styles.notice}>
+            <Image
+              src="/images/icon_notification.svg"
+              width="20"
+              height="20"
+              alt="알림"
+            />
+          </div>
+          <div
+            className={styles.profile}
+            ref={profileRef}
+            onClick={() => setIsProfileCard((prev) => !prev)}
+          >
+            <Image
+              src="/images/no_profileImg.svg"
+              width="32"
+              height="32"
+              alt="프로필 이미지"
+            />
+            <p className={styles.nickname}>정만철</p>
+            {isProfileCard && <NavProfileCard />}
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
