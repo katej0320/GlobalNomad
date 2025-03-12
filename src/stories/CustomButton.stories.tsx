be092a76b1117259388070a0e0d27991cda64f3c
park521 <gh10035@naver.com>
2025-03-12 14:25:46 +0900

    feat: 공용컴포넌트 버튼 추가

---
+++ src/stories/CustomButton.stories.tsx
@@ -0,0 +1,18 @@
+import CustomButton from "../components/CustomButton";
+
+export default {
+  title: "Test/CustomButton",
+  component: CustomButton,
+};
+
+export const test = {
+  args: {
+    children: "로그인 하기",
+    variant: "black",
+    fontSize: "lg",
+    style: {
+      width: "350px",
+      height: "48px",
+    },
+  },
+};
