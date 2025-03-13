4ac1eca658cf9c0a72e6e24e66402164667434ca
HyeyeonJung <jhy20306@gmail.com>
2025-03-14 00:27:08 +0900

    feat: 내 체험 api

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -2,6 +2,7 @@
 
 import NotificationCard from '@/components/notification/NotificationCard';
 import NotificationModal from '@/components/notification/NotificationModal';
+import useMyActivities from '@/hooks/useMyActivities';
 
 export default function MyPage() {
   return (
---
+++ src/hooks/useMyActivities.tsx
@@ -0,0 +1,25 @@
+import { useQuery } from '@tanstack/react-query';
+import instance from '@/lib/api';
+import { Activities } from '@/lib/types';
+
+// API 요청
+const fetchMyActivities = async (): Promise<Activities[]> => {
+  try {
+    const response = await instance.get('/my-activities');
+    console.log('API 응답 데이터:', response.data);
+    return response.data.reservations;
+  } catch (error: unknown) {
+    console.error('API 요청 실패:', error);
+    throw new Error('데이터를 불러오는 데 실패했습니다.');
+  }
+};
+
+// React Query 훅
+const useMyActivities = () => {
+  return useQuery<Activities[]>({
+    queryKey: ['activities'],
+    queryFn: fetchMyActivities,
+  });
+};
+
+export default useMyActivities;
