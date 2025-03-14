29e8ce518d85b66f3b47c78dee141ffc3ebdef8e
HyeyeonJung <jhy20306@gmail.com>
2025-03-14 15:46:49 +0900

    feat: nav 알림 이미지에 알림 모달 연결

--- src/app/ClientLayout.tsx
+++ src/app/ClientLayout.tsx
@@ -15,7 +15,7 @@ export default function ClientLayout({
   const signHideLayout = pathname === '/signin' || pathname === '/signup';
   const mainHideLayout = pathname === '/';
 
-  // ✅ QueryClient가 리렌더링될 때마다 새로 생성되지 않도록 useState 사용
+  // QueryClient가 리렌더링될 때마다 새로 생성되지 않도록 useState 사용
   const [queryClient] = useState(() => new QueryClient());
 
   return (
--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,11 +1,5 @@
 'use client';
 
-import NotificationModal from '@/components/notification/NotificationModal';
-
 export default function MyPage() {
-  return (
-    <>
-      <NotificationModal />
-    </>
-  );
+  return <></>;
 }
