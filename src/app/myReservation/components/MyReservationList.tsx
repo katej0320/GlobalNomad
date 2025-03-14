068f4f3a0053b3e5f08468864684e7855fa28252
Nerim <nerim5037@gmail.com>
2025-03-14 15:49:53 +0900

    feat: 내 예약내역 페이지 UI 구현

---
+++ src/app/myReservation/components/MyReservationList.tsx
@@ -0,0 +1,55 @@
+import CustomButton from '@/components/CustomButton';
+import PageController from './PageController';
+import styles from './style.module.css';
+import Image from 'next/image';
+
+export default function MyReservationList() {
+  const reservationCancelButton: React.CSSProperties = {
+    padding: '8px 20px',
+    background: '#fff',
+    color: '#112211',
+    border: '1px solid #112211',
+    fontWeight: '700',
+  };
+  const writeReviewButton: React.CSSProperties = {
+    padding: '8px 20px',
+    fontWeight: '700',
+  };
+  
+  return (
+    <>
+      <PageController />
+      <div className={styles.listContainer}>
+        <div className={styles.reservationBox}>
+          <div className={styles.thumbnail}>
+            <Image
+              src='/images/testActivity.png'
+              alt='테스트'
+              fill
+              className={styles.img}
+            />
+          </div>
+          <div className={styles.detail}>
+            <div className={styles.top}>
+              <p className={styles.status}>예약 완료</p>
+              <div className={styles.info}>
+                <p className={styles.title}>함께 배우면 즐거운 스트릿 댄스</p>
+                <p className={styles.plan}>
+                  2023. 2. 14<span className={styles.circle}>·</span>11:00 -
+                  12:30
+                  <span className={styles.circle}>·</span>10명
+                </p>
+              </div>
+            </div>
+            <div className={styles.bottom}>
+              <div className={styles.price}>₩10,000</div>
+              <CustomButton style={reservationCancelButton}>
+                후기 작성
+              </CustomButton>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
