7cf675e097e834430ee67aec5bee9c03b99ea7e7
Namgyeon <getam101@naver.com>
2025-03-15 16:40:09 +0900

    fix: build 오류 수정

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,5 +1,49 @@
 'use client';
 
+import { useState, useEffect } from 'react';
+import instance from '@/lib/api';
+import { User } from '@/lib/types';
+import Image from 'next/image';
+import CustomButton from '@/components/CustomButton';
+
 export default function MyPage() {
-  return <></>;
+  const [myProfile, setMyProfile] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  const fetchMyProfile = async () => {
+    try {
+      const response = await instance.get<User>('/users/me');
+      setMyProfile(response.data);
+    } catch (error) {
+      console.log('프로필 데이터를 가져오는 중 오류 발생:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchMyProfile();
+  }, []);
+
+  if (loading) return <div>로딩 중...</div>;
+
+  return (
+    <div>
+      <h1>내 정보</h1>
+      <CustomButton fontSize='sm' variant='black'>
+        수정하기
+      </CustomButton>
+
+      {myProfile?.profileImageUrl && (
+        <Image
+          src={myProfile.profileImageUrl}
+          width={160}
+          height={160}
+          alt='프로필 이미지'
+        />
+      )}
+      <p>닉네임: {myProfile?.nickname}</p>
+      <p>이메일: {myProfile?.email}</p>
+    </div>
+  );
 }
