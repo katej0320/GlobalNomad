f962f809db390887160a38aa5c508811d79e02ab
Nerim <nerim5037@gmail.com>
2025-03-15 15:32:09 +0900

    chore: utils 디렉토리 이동

--- src/utils/useModalController.ts
+++ src/utils/useModalController.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 'use client';
 
 import { useState } from 'react';
@@ -5,11 +6,26 @@ import { useState } from 'react';
 export function useModalController() {
   const [showModal, setShowModal] = useState(false);
   const [isModalMessage, setIsModalMessage] = useState('');
+=======
+import { useState } from "react";
+
+export function useModalController() {
+  const [showModal, setShowModal] = useState(false);
+  const [isModalMessage, setIsModalMessage] = useState("");
+>>>>>>> 56ae002c (chore: utils 디렉토리 이동)
 
   return {
     showModal: showModal ?? false,
     setShowModal: setShowModal ?? (() => {}),
+<<<<<<< HEAD
     isModalMessage: isModalMessage ?? '',
     setIsModalMessage: setIsModalMessage ?? (() => {}),
   };
 }
+=======
+    isModalMessage: isModalMessage ?? "",
+    setIsModalMessage: setIsModalMessage ?? (() => {}),
+  };
+}
+
+>>>>>>> 56ae002c (chore: utils 디렉토리 이동)
