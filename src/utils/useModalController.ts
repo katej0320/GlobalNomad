8ef81729152d4c27dcbbd86ba2f95b3d51a8fb9d
Nerim <nerim5037@gmail.com>
2025-03-16 12:03:09 +0900

    feat: 예약 내역 데이터 출력 및 UX 처리

--- src/utils/useModalController.ts
+++ src/utils/useModalController.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use client';
 
 import { useState } from 'react';
@@ -6,26 +5,13 @@ import { useState } from 'react';
 export function useModalController() {
   const [showModal, setShowModal] = useState(false);
   const [isModalMessage, setIsModalMessage] = useState('');
-=======
-import { useState } from "react";
-
-export function useModalController() {
-  const [showModal, setShowModal] = useState(false);
-  const [isModalMessage, setIsModalMessage] = useState("");
->>>>>>> 56ae002c (chore: utils 디렉토리 이동)
 
   return {
     showModal: showModal ?? false,
     setShowModal: setShowModal ?? (() => {}),
-<<<<<<< HEAD
     isModalMessage: isModalMessage ?? '',
     setIsModalMessage: setIsModalMessage ?? (() => {}),
   };
 }
-=======
-    isModalMessage: isModalMessage ?? "",
-    setIsModalMessage: setIsModalMessage ?? (() => {}),
-  };
-}
 
->>>>>>> 56ae002c (chore: utils 디렉토리 이동)
+
