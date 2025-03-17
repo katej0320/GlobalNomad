1b0cdacc4344df129d748a5bffe7b588f43bd768
yeon0036 <jhy20306@gmail.com>
2025-03-17 16:32:45 +0900

    feature/auth > features (#180)
    * feat: 회원가입, 토스트알림 구현 (#160)
    
    빠르게 머지하겠습니다.
    
    * refactor: input 에러border 추가, passwordInput props(label,id)추가
    
    * fix: build 오류 수정
    
    ---------
    
    Co-authored-by: Namgyeon <getam101@naver.com>

--- src/app/test/namgyeon/page.tsx
+++ src/app/test/namgyeon/page.tsx
@@ -1,37 +1,37 @@
-import styles from "./index.module.css";
-import Input from "@/components/Input/Input";
-import PasswordInput from "@/components/Input/PasswordInput";
-import SelectInput from "@/components/Input/SelectInput";
-import DateInput from "@/components/Input/DateInput";
+import styles from './index.module.css';
+import Input from '@/components/Input/Input';
+import PasswordInput from '@/components/Input/PasswordInput';
+import SelectInput from '@/components/Input/SelectInput';
+import DateInput from '@/components/Input/DateInput';
 
 export default function Page() {
   return (
     <div className={styles.container}>
       <div>
         <Input
-          id="email"
-          type="text"
-          placeholder="이메일을 입력해주세요"
-          label="이메일"
+          id='email'
+          type='text'
+          placeholder='이메일을 입력해주세요'
+          label='이메일'
         />
       </div>
       <div>
         <Input
-          id="email"
-          type="text"
-          placeholder="이메일을 입력해주세요"
-          label="이메일"
-          labelSize="large"
+          id='email'
+          type='text'
+          placeholder='이메일을 입력해주세요'
+          label='이메일'
+          labelSize='large'
         />
       </div>
       <div>
-        <PasswordInput />
+        <PasswordInput id='password' />
       </div>
       <div>
         <SelectInput />
       </div>
       <div className={styles.subContainer}>
-        <DateInput id="date" label="날짜" />
+        <DateInput id='date' label='날짜' />
       </div>
     </div>
   );
