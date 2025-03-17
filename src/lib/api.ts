944ba90c8bbba8a92e209853eff11d39b7e571d7
HyeyeonJung <jhy20306@gmail.com>
2025-03-17 19:37:00 +0900

    build: packages

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -15,7 +15,7 @@ get, post, delete 등 체인방식으로 사용 가능 : instance.get('/users') 
 const instance: AxiosInstance = axios.create({
   baseURL: BASE_URL,
   headers: {
-    Authorization: `Bearer TOKEN`, // << 하드코딩으로 데이터 확인할 때
+    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTc0MywidGVhbUlkIjoiMTItMiIsImlhdCI6MTc0MjIwNjI3MCwiZXhwIjoxNzQyMjA4MDcwLCJpc3MiOiJzcC1nbG9iYWxub21hZCJ9.vOmD5oujzWGj6L-CDZBWph2dbqwahNyNoBNPEvE73zw`, // << 하드코딩으로 데이터 확인할 때
     'Content-Type': 'application/json',
   },
   params: {
