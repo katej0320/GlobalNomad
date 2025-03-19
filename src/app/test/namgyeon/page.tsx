2d92ebb5d552d3f2c37cf0c5013c67cbf781edfa
Namgyeon <getam101@naver.com>
2025-03-19 23:53:55 +0900

    feat: 로그인시 받는 user데이터를 zustand로 전역으로 관리

--- src/app/test/namgyeon/page.tsx
+++ src/app/test/namgyeon/page.tsx
@@ -8,14 +8,17 @@ import DateInput from '@/components/Input/DateInput';
 import Cookies from 'js-cookie';
 import instance from '@/lib/api';
 import { useEffect } from 'react';
+import { useAuthStore } from '@/stores/useAuthStore';
 
 export default function Page() {
   console.log('액세스토큰:', Cookies.get('accessToken'));
   console.log('리프레쉬토큰:', Cookies.get('refreshToken'));
 
+  const { user, logout } = useAuthStore();
+
   const getMyInfo = async () => {
     const response = await instance.get('/users/me');
-    console.log(response.data);
+    console.log('api 나의 데이터 조회', response.data);
   };
 
   useEffect(() => {
@@ -53,6 +56,18 @@ export default function Page() {
       <div className={styles.subContainer}>
         <DateInput id='date' label='날짜' />
       </div>
+      <div>
+        {user ? (
+          <>
+            <p>{user.id}</p>
+            <p>{user.email}</p>
+            <p>{user.nickname}</p>
+          </>
+        ) : (
+          <p>로그인 필요</p>
+        )}
+      </div>
+      <button onClick={() => logout()}>로그아웃</button>
     </div>
   );
 }
