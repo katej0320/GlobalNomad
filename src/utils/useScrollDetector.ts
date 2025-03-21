033c62f10f1d2f0f6f9d6128b8f2f11cdb9607cd
Nerim <nerim5037@gmail.com>
2025-03-21 17:47:38 +0900

    feat: 무한스크롤 기능 구현 및 로딩 UI 수정

--- src/utils/useScrollDetector.ts
+++ src/utils/useScrollDetector.ts
@@ -1,10 +1,17 @@
 import { useEffect } from 'react';
 
+<<<<<<< HEAD
 export const useScrollDetector = (callback: () => void) => {
   useEffect(() => {
     const handleScroll = () => {
       const { scrollHeight, scrollTop, clientHeight } =
         document.documentElement;
+=======
+const useScrollDetector = (callback: () => void) => {
+  useEffect(() => {
+    const handleScroll = () => {
+      const { scrollHeight, scrollTop, clientHeight } = document.documentElement;
+>>>>>>> 888ae99a (feat: 무한스크롤 기능 구현 및 로딩 UI 수정)
       if (scrollTop + clientHeight >= scrollHeight - 10) {
         callback();
       }
@@ -14,3 +21,8 @@ export const useScrollDetector = (callback: () => void) => {
     return () => window.removeEventListener('scroll', handleScroll);
   }, [callback]);
 };
+<<<<<<< HEAD
+=======
+
+export default useScrollDetector;
+>>>>>>> 888ae99a (feat: 무한스크롤 기능 구현 및 로딩 UI 수정)
